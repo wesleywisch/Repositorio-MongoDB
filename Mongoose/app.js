f9d4aa5704b2a2180fb9c87d3e6421e9cd828633
wesley_wisch <wesley_wisch@hotmail.com>
2021-05-27 13:15:07 -0400

    Aprendendo a criar um documento

--- Mongoose/app.js
+++ Mongoose/app.js
@@ -3,6 +3,10 @@ const app = express();
 const port = 3000
 const mongoose = require('mongoose');
 
+
+// As 3 maneiras de se conectar no banco de dados.
+
+
 // 1° Maneira de se conectar no banco de dados é através de um callback
 
 // dentro do connect colocaremos a url para termos conecção com o banco de dados de fato, mais usaremos o localhost, mais para frente colocamos relamente uma url de servidor.
@@ -25,11 +29,75 @@ const mongoose = require('mongoose');
 
 // 3° Maneira de se conectar no banco de dados.
 
-mongoose.connect("mongodb://localhost/links", { useNewUrlParser: true, useUnifiedTopology: true})
+// mongoose.connect("mongodb://localhost/links", { useNewUrlParser: true, useUnifiedTopology: true})
+
+// let db = mongoose.connection;
+
+// // console.log(db);
+
+// db.on("error", () => { console.log("houve um erro") });
+// db.once("open", () => { console.log("Banco carregado") });
+
+
+// Criando um documento
+
+
+// documento criando usando uma forma de links
+
+// o Schema é o jeito que vai conter o nosso documento.
+
+// const linkSchema = new mongoose.Schema({
+//     title: {type: String, required: true}, // o required siginifica que o campo é obrigatorio estar preenchido.
+//     description: String,
+//     url: {type: String, required: true},
+//     click: {type: Number, default: 0},
+// });
+
+
+// o model ele é mais a representação da nossa coleção 
+
+// const Link = mongoose.model('Link', linkSchema);
+
+// let link = new Link({
+//     title: "Github",
+//     description: "Link para o Github",
+//     url: "https://github.com/wesleywisch",
+//     // click: 0,
+// });
+
+// link.save().then(doc => { // serve para salver no banco de dados
+//     console.log(doc);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+
+// documento criado usando forma de pessoas
+
+// const personSchema = new mongoose.Schema({
+//     name: String,
+//     age: Number,
+// });
+
+// const Person = mongoose.model('Person', personSchema);
+
+// let person = new Person({
+//     name: "José",
+//     age: 23,
+// });
+
+// person.save().then(doc => {
+//     console.log(doc);
+// }).catch(err =>{
+//     console.log(err);
+// });
+
+
+
+mongoose.connect("mongodb://localhost/newlinks", { useNewUrlParser: true, useUnifiedTopology: true })
 
 let db = mongoose.connection;
 
-// console.log(db);
 
 db.on("error", () => { console.log("houve um erro") });
 db.once("open", () => { console.log("Banco carregado") });
