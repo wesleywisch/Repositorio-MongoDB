d55efd46709390d4350aaf2623095e1a754fb641
wesley_wisch <wesley_wisch@hotmail.com>
2021-05-31 08:04:59 -0400

    Forma de apagar um link/ajustando as rotas

--- Mongoose/controlles/linkController.js
+++ Mongoose/controlles/linkController.js
@@ -3,14 +3,18 @@ const Link = require("../models/Link");
 // aqui aonde vai ter todas as funções
 
 
-const redirect = async (req, res) => {
+const redirect = async (req, res, next) => {
 
     let title = req.params.title;
 
     try {
         let doc = await Link.findOne({ title }) // o find me retorna todos os objetos dentro do array, eo o findOne me retorna somente um, mais com o que condiz com o que foi passado no casso o (title).
         console.log(doc);
-        res.redirect(doc.url);
+        if(doc){
+            res.redirect(doc.url);
+        }else{
+            next();
+        }
     } catch (error) {
         res.send(error);
     }
@@ -22,7 +26,7 @@ const addLink = async (req, res) => {
 
     try {
         let doc = await link.save();
-        res.send("Link adicionado com sucesso!");
+        res.redirect('/');
     } catch (error) {
         res.render('index', { error, body: req.body });
     }
@@ -47,7 +51,8 @@ const deleteLink = async(req, res) =>{
 
     try {
         await Link.findByIdAndDelete(id);
-        res.send(id);
+        // res.send(id);
+        res.redirect('/')
     } catch (error) {
         res.status(404).send(error);
     }
--- Mongoose/routes/linkRouter.js
+++ Mongoose/routes/linkRouter.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const linkController = require('../controlles/linkController');
+const methodOverride = require('method-override');
 
+router.use(methodOverride('_method'));
 
-router.get('/all', linkController.allLinks); // essa rota retona todos os links que tiverem salvo no banco
+router.get('/', linkController.allLinks); // essa rota retona todos os links que tiverem salvo no banco
 
 router.get('/:title', linkController.redirect); // se houver um erro ele executa essa parte
 
-router.get("/", (req, res) => res.render('index', { error: false, body: {} })); // se não houver nenhum erro ele executa essa parte
+router.get("/add", (req, res) => res.render('index', { error: false, body: {} })); // se não houver nenhum erro ele executa essa parte
 
 router.post('/', express.urlencoded({ extended: true }), linkController.addLink);
 
 router.delete('/:id', linkController.deleteLink);
 
-router.delete('/', express.json(), linkController.deleteLink);
+router.delete('/', express.urlencoded({extended: true}), linkController.deleteLink);
 
 
 module.exports = router
