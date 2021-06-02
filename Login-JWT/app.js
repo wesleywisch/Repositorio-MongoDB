87145b87d50a629d46a8a653617ad50b4817799a
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-02 08:15:37 -0400

    configurando o moongose para se conectar no bando de dados(MongoDB)

--- Login-JWT/app.js
+++ Login-JWT/app.js
@@ -2,6 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRouter = require('./routes/router');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_CONNECTION_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    (error) => {
+        if (error) {
+            console.log(error)
+        }else {
+            console.log("Mongo conectado")
+        }
+    });
 
 app.use('/user', userRouter)
 
