6cbe86ba963e747acc6448149ef78d38efac650b
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-04 08:56:13 -0400

    Criando um chat. Configurando a base

---
+++ Chat/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+
+app.listen(3000, ()=>{
+    console.log("Server rodando");
+})
