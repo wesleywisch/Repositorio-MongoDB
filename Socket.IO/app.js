96e40cdffebd761c0f160efefc7db8a33ea65ab3
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-04 07:55:32 -0400

    Integrando o front com o back pelo socketIO

---
+++ Socket.IO/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const socketIO = require('socket.io');
+
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+
+const server = app.listen(3000, () =>{
+    console.log("Servidor rodando!")
+});
+
+const io = socketIO(server);
+
+
+io.on('connection', (socket)=>{
+
+    console.log("Nova conecção");
+
+})
